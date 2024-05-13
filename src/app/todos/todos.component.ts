92f91469abc5673e026378b38536d191cc2affbb
Danzoniel <danigaara.ds@gmail.com>
2024-05-13 18:48:21 -0300

    feat: adding authenticator with jwt

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -35,6 +35,7 @@ export class TodosComponent implements OnInit {
     try {
       client.models.Todo.create({
         content: window.prompt('Todo content'),
+        isDone: false,
       });
       this.listTodos();
     } catch (error) {
