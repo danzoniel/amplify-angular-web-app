3355bfaaf3fc14379ec536ab3b74312f79df35a2
Danzoniel <danigaara.ds@gmail.com>
2024-05-26 22:52:35 -0300

    feat(wip): adding front ent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TodosComponent } from './todos/todos.component';
+import { HeaderComponent } from './header/header.component';
 import { Amplify } from 'aws-amplify';
 import outputs from '../../amplify_outputs.json';
 import { AmplifyAuthenticatorModule, AuthenticatorService } from '@aws-amplify/ui-angular';
@@ -8,12 +9,13 @@ import { AmplifyAuthenticatorModule, AuthenticatorService } from '@aws-amplify/u
 Amplify.configure(outputs);
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', 
   standalone: true,
-  templateUrl: 'header/header.component.html',
-  styleUrl: 'header/header.component.css',
-  imports: [RouterOutlet, TodosComponent, AmplifyAuthenticatorModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [TodosComponent, HeaderComponent, AmplifyAuthenticatorModule],
 })
+
 export class AppComponent {
   title = 'amplify-angular-template';
   constructor(public authenticator: AuthenticatorService) {
