3e88de5e4358075abbfa49e23ef28aa39830ce8e
Danzoniel <danigaara.ds@gmail.com>
2024-06-02 18:41:30 -0300

    feat(wip): adding area chart

--- src/app/app-chart/app-chart.component.ts
+++ src/app/app-chart/app-chart.component.ts
@@ -1,13 +1,34 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
+import { EChartsOption } from 'echarts';
 
 @Component({
   selector: 'app-chart',
   standalone: true,
-  imports: [CommonModule],
-  template: `<div>Chart Component</div>`, // Substitua com o template real do seu gráfico
+  imports: [CommonModule, NgxEchartsDirective],
+  templateUrl: './app-chart.component.html',
+  styleUrl: './app-chart.component.css',
+  providers: [
+    provideEcharts(),
+  ]
 })
 export class AppChartComponent {
-  @Input() data: any;
-  @Input() options: any;
+  chartOption: EChartsOption = {
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line',
+        areaStyle: {}
+      },
+    ],
+  };
 }
