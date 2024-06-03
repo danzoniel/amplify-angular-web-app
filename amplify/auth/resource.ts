4a363f1f6e4989806af1ff89598cceb1ac373189
Danzoniel <danigaara.ds@gmail.com>
2024-06-03 03:53:27 -0300

    feat: test removing google auth provider

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -2,19 +2,19 @@ import { defineAuth, secret } from '@aws-amplify/backend';
 
 export const auth = defineAuth({
   loginWith: {
-    externalProviders: {
-      google: {
-        clientId: secret('GOOGLE_CLIENT_ID'),
-        clientSecret: secret('GOOGLE_CLIENT_SECRET'),
+  //   externalProviders: {
+  //     google: {
+  //       clientId: secret('GOOGLE_CLIENT_ID'),
+  //       clientSecret: secret('GOOGLE_CLIENT_SECRET'),
 
-        scopes: ['email']
-      },
-      callbackUrls: [
-        'http://localhost:4200/callback',
-        'https://develop.d1uu1zzq9swwnn.amplifyapp.com/callback'
-      ],
-      logoutUrls: ['http://localhost:4200/signout', 'https://develop.d1uu1zzq9swwnn.amplifyapp.com/signout'],
-    },
+  //       scopes: ['email']
+  //     },
+  //     callbackUrls: [
+  //       'http://localhost:4200/callback',
+  //       'https://develop.d1uu1zzq9swwnn.amplifyapp.com/callback'
+  //     ],
+  //     logoutUrls: ['http://localhost:4200/signout', 'https://develop.d1uu1zzq9swwnn.amplifyapp.com/signout'],
+  //   },
     email: true,
   }
 });
